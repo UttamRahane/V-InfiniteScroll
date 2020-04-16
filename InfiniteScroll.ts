72bbc6735012c57ab66bc2669c5988a9c82cc3a9
UttamRahane <uttamrahane@gmail.com>
2020-04-16 19:40:01 +0530

    Added v-InfiniteScroll directive

---
+++ InfiniteScroll.ts
@@ -0,0 +1,18 @@
+import { DirectiveOptions } from 'vue';
+
+const directive: DirectiveOptions = {
+    inserted(el, binding, node: any) {
+        const callback = binding.value;
+        const fun = () => {
+            const visible = node.elm.scrollTop;
+            const pageHeight = node.elm.scrollHeight;
+            const limit = (pageHeight / 100) * 90; // 90% of page height
+            if (visible > limit) {
+                callback();
+            }
+        };
+        el.addEventListener('scroll', fun);
+    },
+};
+
+export default directive;
